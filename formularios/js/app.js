971a6a30d9d7dff3b2302e552334aad799dc0c2a
Roberto Romero <roberto@ulama.co>
2022-08-15 21:30:58 -0500

    formularios

--- formularios/js/app.js
+++ formularios/js/app.js
@@ -1,3 +1,61 @@
+let arreglo = [];
+
+let agregar = (evt) => {
+    evt.preventDefault();
+    //console.log(evt.target);  
+    const {nombre, numero, foto, peso} = evt.target;
+    let pokemon ={
+        "nombre": nombre.value,
+        "numero": numero.value,
+        "foto": foto.value,
+        "peso": peso.value
+    };
+
+    arreglo.push(pokemon)
+    evt.target.reset() // Es lo mismo que esto:     
+    //document.querySelector("#formulario").reset()
+    dibujar();
+    //console.log(`Nombre: ${pokemon.nombre} con Numero: ${pokemon.numero} se ha agreado a la lista`);
+}
+
+let dibujar = () =>{
+
+    document.querySelector("#resultados").innerHTML="";
+    /*
+    for(let i=0 ; i<arreglo.length ; i++){
+        console.log(arreglo[i].nombre);
+    }
+
+    arreglo.forEach(function(element){
+        console.log(element.nombre);
+    })
+    
+    for(let element of arreglo){
+        console.log(element.nombre);
+    }*/
+
+    for(let i in arreglo){
+        let pokemon = arreglo[i];
+        let card = `<article class="media has-background-white p-3">
+            <figure class="media-left">
+            <p class="image is-64x64">
+                <img src="${pokemon.foto}">
+            </p>
+            </figure>
+            <div class="media-content">
+            <div class="content">
+                <h4 class="mb-2"><strong>${pokemon.nombre}</strong> <small>${pokemon.numero}</small></h4>
+                <p>${pokemon.peso} kg</p>
+            </div>
+            </div>
+        </article>`;
+
+
+        document.querySelector("#resultados").innerHTML += card;
+    }    
+
+}
+
 function agregar_old(evt){
     evt.preventDefault();
     let nombre = document.querySelector("#nombre").value;
@@ -24,18 +82,6 @@ function agregar_old(evt){
 }
 
 
-let agregar2 = (evt) => {
-    evt.preventDefault();
-    //console.log(evt.target);  
-    const {nombre, numero, foto, peso} = evt.target;
-    let pokemon ={
-        "nombre": nombre.value,
-        "numero": numero.value,
-        "foto": foto.value,
-        "peso": peso.value
-    };
-    console.log(`Nombre: ${pokemon.nombre} con Numero: ${pokemon.numero} se ha agreado a la lista`);
-}
 
 document.querySelector("#formulario").addEventListener("submit",agregar);
 
